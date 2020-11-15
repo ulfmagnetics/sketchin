9189af6a4a78308f6e96bed38b418cd44697b071
Glitch (hello-webpage) <none>
2020-11-15 19:33:16 +0000

    🐜🙎 Checkpoint ./src/js/matrix.js:1236437/73 ./src/css/matrix.scss:1236437/192 ./src/css/main.scss:1236437/61

--- src/js/matrix.js
+++ src/js/matrix.js
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types';
 import Pixel from './pixel';
 
 function initialData({ rows, cols }) {
-  return Array(cols).fill({ r: 0, g: 0, b: 0 }).map(() => new Array(rows).fill({ r: 0, g: 0, b: 0 }));
+  return Array(rows).fill({ r: 0, g: 0, b: 0 }).map(() => new Array(cols).fill({ r: 0, g: 0, b: 0 }));
 }
 
-function renderColumn(row, x) {
+function renderRow(row, x) {
   return row.map((rgb, y) => (
     <Pixel key={`${x}-${y}`} x={x} y={y} r={rgb.r} g={rgb.g} b={rgb.b} />
   ));
@@ -18,9 +18,8 @@ function Matrix(props) {
     
   return (
     <div className='matrix-container'>
-      <h1>welcome to the matrix</h1>
       <div className='matrix-grid'>
-        {data.flatMap((row, x) => renderColumn(row, x))}
+        {data.flatMap((row, x) => renderRow(row, x))}
       </div>
     </div>
   );
