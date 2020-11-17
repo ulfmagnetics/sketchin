a7ba4b055dd3c89b8492af414b05955587231f3d
Glitch (hello-webpage) <none>
2020-11-17 02:16:19 +0000

    🏘🛰 Checkpoint ./src/js/utils.js:1236437/1230

--- src/js/utils.js
+++ src/js/utils.js
@@ -8,7 +8,14 @@ export function fillWithGradient(rows, cols) {
   const data = fillGrid(rows, cols, 0, 0, 0);
   for (var col of range(cols)) {
     for (var row of range(rows)) {
-      data[row][col] =  
+      const color1 = { r: 255, g: 0, b: 0 };
+      const color2 = { r: 0, g: 0, b: 255 };
+      const rowPercent = row / rows;
+      const colPercent = col / cols;
+      const r = color1.r + rowPercent * (color2.r - color1.r);
+      const g = color1.g + ((rowPercent + colPercent) / 2) * (color2.g - color1.g);
+      const b = color1.b + colPercent * (color2.b - color1.b);
+      data[row][col] = { r, g, b }; 
     }
   }
   return data;
