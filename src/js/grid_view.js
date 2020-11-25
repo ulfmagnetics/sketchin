f1c3ff99aafef24bb9bcfca055e9d1c9490cfcc5
Glitch (hello-webpage) <none>
2020-11-25 02:24:18 +0000

    💇💧 Checkpoint ./src/js/grid_view.js:1236437/268

--- src/js/grid_view.js
+++ src/js/grid_view.js
@@ -3,11 +3,16 @@ import React, { useState, useEffect } from 'react';
 import Matrix from './matrix';
 import Controls from './controls';
 import { fillGrid, fillWithGradient } from './utils';
+import { bmp_rgb } from './lib/jsbmp';
 
 function GridView() {
   const [numRows, setNumRows] = useState(32);
   const [numCols, setNumCols] = useState(64);
   const [matrixData, setMatrixData] = useState(fillGrid(numRows, numCols, 0, 0, 0));
+  
+  useEffect(() => {
+    // TODO: do something with the matrixData here?
+  });
   
   const onControlClicked = (e) => {
     const controlType = e.target.getAttribute('data-control-type');
@@ -18,6 +23,9 @@ function GridView() {
     }
     else if (controlType == 'reset') {
       setMatrixData(fillGrid(numRows, numCols, 0, 0, 0));
+    }
+    else if (controlType == 'publish') {
+      
     }
   };
   
