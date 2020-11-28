001c14b486ba12dfb7f5403b31f23d1be545399b
Glitch (hello-webpage) <none>
2020-11-28 15:15:35 +0000

    🏡💁 Checkpoint ./package.json:1236437/2771 ./src/js/main.js:1236437/237

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Amplify, { Auth } from 'aws-amplify';
+import awsconfig from './aws-exports';
 
 import GridPage from './ui/pages/GridPage';
 
 import '../css/main.scss';
+
+Amplify.configure(awsconfig);
 
 // TODO: expose matrix size props in the UI
 
