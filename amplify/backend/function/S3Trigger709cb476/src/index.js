297da82bb3f9bb70c69a9cf5e966c0b18430052c
John Berry <ulfmagnetics@gmail.com>
2021-01-02 15:19:34 -0500

    Add basics of Lambda trigger for processing images

--- amplify/backend/function/S3Trigger709cb476/src/index.js
+++ amplify/backend/function/S3Trigger709cb476/src/index.js
@@ -1,9 +1,27 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
 // eslint-disable-next-line
-exports.handler = function(event, context) {
+exports.handler = async (event, context, callback) => {
   console.log('Received S3 event:', JSON.stringify(event, null, 2));
   // Get the object from the event and show its content type
   const bucket = event.Records[0].s3.bucket.name; //eslint-disable-line
   const key = event.Records[0].s3.object.key; //eslint-disable-line
   console.log(`Bucket: ${bucket}`, `Key: ${key}`);
-  context.done(null, 'Successfully processed S3 event'); // SUCCESS with message
+
+  const params = {
+    Bucket: event.Records[0].s3.bucket.name,
+    Key: event.Records[0].s3.object.key,
+  };
+  console.log(params);
+
+  try {
+    const image = await s3.getObject(params).promise();
+    console.log('image', image);
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+
+  //context.done(null, 'Successfully processed S3 event'); // SUCCESS with message
 };
--- src/js/ui/components/Controls.js
+++ src/js/ui/components/Controls.js
@@ -2,14 +2,27 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { AmplifySignOut } from '@aws-amplify/ui-react';
 
+function onFileUpload() {
+
+}
+
 function Controls(props) {
   const { onClick } = props;
+  const [imageFile, setImageFile] = useState();
+
+  const onFileChange = (e) => {
+    setImageFile(e.target.files[0]);
+  };
 
   return (
     <ul className='controls'>
       <li><button data-control-type='gradient1' onClick={onClick}>Gradient 1</button></li>
       <li><button data-control-type='gradient2' onClick={onClick}>Gradient 2</button></li>
       <li><button data-control-type='reset' onClick={onClick}>Reset</button></li>
+      <li>
+        <input type="file" onChange={onFileUpload} />
+        <button data-control-type='import'>Import</button>
+      </li>
       <li><button data-control-type='publish' onClick={onClick}>Publish</button></li>
       <li><AmplifySignOut /></li>
     </ul>
