0f66d09fa3bd913f328a081df1b5a63cd394a65a
John Berry <ulfmagnetics@gmail.com>
2021-01-03 10:19:03 -0500

    Implement image resizing in the S3 trigger

--- amplify/backend/function/S3Trigger709cb476/src/index.js
+++ amplify/backend/function/S3Trigger709cb476/src/index.js
@@ -1,27 +1,64 @@
 const AWS = require('aws-sdk');
-const s3 = new AWS.S3();
+const S3 = new AWS.S3();
+const Sharp = require('sharp');
+const _chunk = require('lodash.chunk');
+
+const { bmp_rgb } = require('./jsbmp.js');
+
+// TODO: pull these in via env variables
+const MATRIX_WIDTH = 64;
+const MATRIX_HEIGHT = 32;
+
+// based on https://amplify-workshop.go-aws.com/70_generating_thumbnails/10_processing_photos_with_the_S3_trigger_lambda_function.html
+
+const zeroPad = (num, padding = 2) => ("0".repeat(padding) + num).slice(-padding);
+const toHex = (i) => zeroPad(i.toString(16));
+
+async function processRecord(record) {
+  const bucketName = record.s3.bucket.name;
+  const key = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
+  console.log('processRecord', JSON.stringify(record))
+
+  if (record.eventName !== "ObjectCreated:Put") {
+    console.log('Is not a new file');
+    return;
+  }
+
+  if (!key.includes('upload/')) {
+    console.log('Does not look like an upload from user');
+    return;
+  }
+
+  const image = await S3.getObject({ Bucket: bucketName, Key: key }).promise();
+
+  // Note that we're "flopping" the image around its horizontal X axis since bmp_rgb
+  // expects the data to be stored bottom-to-top.
+  const buffer = await Sharp(image.Body).resize(MATRIX_WIDTH, MATRIX_HEIGHT).flop().raw().toBuffer();
+
+  const rgbs = _chunk(Array.from(buffer), 3);
+  const pixels = rgbs.map((rgb) => rgb.map(toHex).join(''));
+
+  const data = bmp_rgb(MATRIX_WIDTH, MATRIX_HEIGHT, pixels);
+
+  // FIXME: need to set the public-read ACL on new object (may require new permission for Lambda function)
+
+  const keyPrefix = key.substr(0, key.indexOf('/upload/'));
+  await S3.putObject({
+    Body: Buffer.from(data, 'binary'),
+    Bucket: bucketName,
+    Key: `${keyPrefix}/imported/test.bmp`, // TODO: use environment variable with UUID
+  }).promise();
+}
 
 // eslint-disable-next-line
 exports.handler = async (event, context, callback) => {
   console.log('Received S3 event:', JSON.stringify(event, null, 2));
-  // Get the object from the event and show its content type
-  const bucket = event.Records[0].s3.bucket.name; //eslint-disable-line
-  const key = event.Records[0].s3.object.key; //eslint-disable-line
-  console.log(`Bucket: ${bucket}`, `Key: ${key}`);
-
-  const params = {
-    Bucket: event.Records[0].s3.bucket.name,
-    Key: event.Records[0].s3.object.key,
-  };
-  console.log(params);
 
   try {
-    const image = await s3.getObject(params).promise();
-    console.log('image', image);
+    event.Records.forEach(processRecord);
+    callback(null, { status: 'Image Processed' });
   } catch (error) {
     console.log(error);
-    return;
+    callback(error);
   }
-
-  //context.done(null, 'Successfully processed S3 event'); // SUCCESS with message
 };
---
+++ amplify/backend/function/S3Trigger709cb476/src/jsbmp.js
@@ -0,0 +1,117 @@
+/* jsbmp
+
+Create bitmap files using JavaScript. The use-case this was written
+for is to create simple images which can be provided in `data` URIs.
+
+Copyright (c) 2009 Sam Angove <sam [a inna circle] rephrase [period] net>
+
+License: MIT
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+/*
+Create the binary contents of a bitmap file.
+
+This is not a public interface and is subject to change.
+
+Arguments:
+
+    width -- width of the bitmap
+    height -- height of the bitmap
+    palette -- array of 'rrggbb' strings (if appropriate)
+    imgdata -- pixel data in faux-binary escaped text
+    bpp -- bits per pixel; use in conjunction with compression
+    compression -- compression mode (e.g. uncompressed, 8-bit RLE, 4-bit RLE)
+*/
+/* eslint-disable */
+function _bmp(width, height, palette, imgdata, bpp, compression) {
+
+    var imgdatasize = imgdata.length;
+    var palettelength = palette.length;
+    var palettesize = palettelength * 4; // 4 bytes per colour
+    var filesize = 64 + palettesize + imgdatasize; // size of file
+    var pixeloffset = 54 + palettesize; // pixel data offset
+    var data = [
+        "BM",                                 // magic number
+        _pack(width),      // size of file
+        "\x00\x00\x00\x00",               // unused
+        _pack(pixeloffset),   // number of bytes until pixel data
+        "\x28\x00\x00\x00",               // number of bytes left in the header
+        _pack(width),         // width of pixmap
+        _pack(height),        // height of pixmap
+        "\x01\x00",                         // number of colour planes, must be 1
+        _pack(bpp, 2),           // bits per pixel
+        _pack(compression),   // compression mode
+        _pack(imgdatasize),   // size of raw BMP data (after the header)
+        "\x13\x0B\x00\x00",               // # pixels per metre horizontal res.
+        "\x13\x0B\x00\x00",               // # pixels per metre vertical res
+        _pack(palettelength), // num colours in palette
+        "\x00\x00\x00\x00"                // all colours are important
+
+        // END OF HEADER
+     ];
+
+    for (var i=0; i<palette.length; ++i) {
+        data.push(_pack(parseInt(palette[i], 16)));
+    }
+    data.push(imgdata);
+    return data.join("");
+}
+/*
+Pack JS integer (signed big-endian?) `num` into a little-endian binary string
+of length `len`.
+*/
+function _pack(num, len) {
+    var o = [], len = ((typeof len == 'undefined') ? 4 : len);
+    for (var i=0; i<len; ++i) {
+        o.push(String.fromCharCode((num >> (i * 8)) & 0xff));
+    }
+    return o.join("");
+}
+
+
+/*
+Create an uncompressed Windows bitmap (BI_RGB) given width, height and an
+array of pixels.
+
+Pixels should be in BMP order, i.e. starting at the bottom left, going up
+one row at a time.
+
+Example:
+
+    var onebluepixel = bmp(1, 1, ['0000ff']);
+*/
+function bmp_rgb(width, height, pixarray) {
+    var rowsize = (width * 3);
+    var rowpadding = (rowsize % 4);
+    if (rowpadding) rowpadding = Math.abs(4 - rowpadding);
+    var imgdatasize = (rowsize + rowpadding) * height;
+
+    var i, j, pix;
+    var pixcache = {};
+    // Based on profiling, it's more than 10x faster to reverse the array
+    // and pop items off the end than to shift them of the front. WTF.
+    pixarray.reverse();
+    var pixels = [];
+    for (i=0; i<height; ++i) {
+        for (j=0; j<width; ++j) {
+            pix = pixarray.pop();
+            if (typeof pixcache[pix] == 'undefined')
+                pixcache[pix] = _pack(parseInt(pix, 16), 3);
+            pixels.push(pixcache[pix]);
+        }
+        for (j=0; j<rowpadding; ++j) {
+            pixels.push("\x00");
+        }
+    }
+    return _bmp(width, height, [], pixels.join(""), 24, 0);
+}
+
+exports.bmp_rgb = bmp_rgb;
